a0c9bed44a7a798b778ac7540acf408f38048142
AmitMizrahi123 <amitmizrahi231055@gmail.com>
2021-04-18 10:14:33 +0300

    adding comments

--- main.cpp
+++ main.cpp
@@ -7,19 +7,20 @@
 #include <algorithm>
 using namespace std;
 
-int probabilityOfAddingEdgeToGraph(double p)
+int probabilityOfAddingEdgeToGraph(double p) // Function for get a random probability for adding or not to the graph's edge
 {
-	double r = ((double)rand() / (RAND_MAX)); // Get random number between 0-1
+	double r = ((double)rand() / (RAND_MAX)); // Get random number between 0 to 1
 	return p > r;
 }
 
-void addEdge(int a, int b, vector<vector<int>> &edge)
+void addEdge(int a, int b, vector<vector<int>> &edge) // Function for adding edge to the graph
 {
+	// Because we have an undircted graph, we need to push edge for a and b.
 	edge[a].push_back(b);
 	edge[b].push_back(a);
 }
 
-vector<vector<int>> ‫‪build_random_graph‬‬(int n, double p)
+vector<vector<int>> ‫‪build_random_graph‬‬(int n, double p) // Function for building a random graph
 {
 	int i, j, maxEdges = p * (n * (n - 1) / 2), countEdges = 0;
 	vector<vector<int>> edge(n, vector<int>());
@@ -36,7 +37,7 @@ vector<vector<int>> ‫‪build_random_graph‬‬(int n, double p)
 	return edge;
 }
 
-vector<int> BFS(int v, vector<vector<int>> edge)
+vector<int> BFS(int v, vector<vector<int>> edge) // BFS fucntion to get victor of distance
 {
 	int u, i, n = edge.size();
 	vector<int> dist(n, 0);
@@ -58,21 +59,25 @@ vector<int> BFS(int v, vector<vector<int>> edge)
 	return dist;
 }
 
+/* Function that check if graph is connectivity by running one time BFS and check the vector distance */
 int connectivity(vector<vector<int>> edge)
 {
 	int n = edge.size();
 	vector<int> dist = BFS(0, edge);
 	for (int i = 1; i < n; i++) if (dist[i] < 1) return 0;
+	/* We will get dist[i]<1 this means that there is no path from the start vertex to the same vertex -> not connectivity*/
 	return 1;
 }
 
+/* Function that check if every vertex in the garph is isolated */
 int Isolated_Is(vector<vector<int>> edge)
 {
 	int n = edge.size();
-	for (int i = 0; i < n; i++) if (!edge[i].size()) return 1;
+	for (int i = 0; i < n; i++) if (!edge[i].size()) return 1; // If the vector's size is 0 this means that the vertex is isolated
 	return 0;
 }
 
+/* Function that runs BFS on all vertices, to get the maximum distance that is the diameter */
 int diameter(vector<vector<int>> edge)
 {
 	int n = edge.size();
@@ -80,13 +85,15 @@ int diameter(vector<vector<int>> edge)
 	int diam = 0, temp;
 	if (!connectivity(edge)) return -1;
 	for (int i = 0; i < n; i++) {
-		dist = BFS(i, edge);
-		temp = int(*max_element(dist.begin(), dist.end()));
-		diam = max(temp, diam);
+		dist = BFS(i, edge); // Get the vector distance 
+		temp = int(*max_element(dist.begin(), dist.end())); // Save the maximum distance in vector of distance
+		diam = max(temp, diam); // Check who is biggest
 	}
 	return diam;
 }
 
+// Function that check connectivity by build 5000 random graphs and check the attributes by is threshold.
+// We save the result of every graph in csv file.
 void checkConnectivity(double arr[], int n, double threshold1and3)
 {
     fstream fout;
@@ -118,6 +125,8 @@ void checkConnectivity(double arr[], int n, double threshold1and3)
     fout.close();
 }
 
+// Function that check isolated by build 5000 random graphs and check the attributes by is threshold.
+// We save the result of every graph in csv file.
 void checkIsolated_Is(double arr[], int n, double threshold1and3)
 {
 	fstream fout;
@@ -149,6 +158,8 @@ void checkIsolated_Is(double arr[], int n, double threshold1and3)
     fout.close();
 }
 
+// Function that check diameter by build 5000 random graphs and check the attributes by is threshold.
+// We save the result of every graph in csv file.
 void checkDiameter(double arr[], int n, double threshold2)
 {
 	fstream fout;
